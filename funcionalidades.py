55889ee1555df956b9f6b2cafc3833bceee326d1
marcelakramer <marcelabarretokramer10@gmail.com>
2022-06-28 22:36:20 -0300

    retirando a opção 'configurações' do menu

--- funcionalidades.py
+++ funcionalidades.py
@@ -1,14 +1,12 @@
 import os, animations
 
 
-
 def imprime_menu_principal():
     animations.loading(0.1)
     os.system('clear')
     print(f'Escolha entre as opções a seguir:\n')
     print(f'Novo Jogo       -> 1')
-    print(f'Configurações   -> 2')
-    print(f'Encerrar        -> 3')
+    print(f'Encerrar        -> 2')
     opcao = int(input(f'\nDigite opção desejada: '))
     os.system('clear')
     return opcao
--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import os, time, tabuleiro, animations, funcionalidades
 # Arquivo principal
 print('Batalha Naval')
 
-modo_jogo = 'normal'
-tamanho_tabuleiro = 10
-
 while(True):
     
     opcao = funcionalidades.imprime_menu_principal()
@@ -15,20 +12,12 @@ while(True):
         animations.loading(0.1)
         os.system('clear')
         print(f'Início do jogo \n')
-        nome_jogador1 = input(f'nome do jogador 1: ')
-        nome_jogador2 = input(f'nome do jogador 2: ')
+        nome_jogador1 = input(f'Nome do jogador 1: ')
+        nome_jogador2 = input(f'Nome do jogador 2: ')
         tabuleiro_jogador1 = tabuleiro.cria_tabuleiro()
         tabuleiro_jogador2 = tabuleiro.cria_tabuleiro()
         tam_frota = int(input(f'De quantos Navios será formada a frota de cada jogador? '))
     elif(opcao == 2):
-        animations.loading(0.1)
-        os.system('clear')
-        print(f'Configurações:\n')
-        print(f'( 1 ) Modo de jogo:       {modo_jogo}')
-        print(f'( 2 )Tamanho tabuleiro:  {tamanho_tabuleiro}x{tamanho_tabuleiro}')
-        config = int(input((f'O que deseja fazer: ')))
-
-    elif(opcao == 3):
         animations.loading(0.1)
         os.system('clear')
         print(f'ESPERO QUE TENHA SE DIVERTIDO!!!!!\n ATÉ A PRÓXIMA!!!!!')
